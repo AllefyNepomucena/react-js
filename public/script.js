23bac9f50d837111cf2d5aab2af6f525135feb27
AllefyNepomucena <allefy.s.nepomucena@gmail.com>
2021-11-01 22:02:47 -0300

    Erro Do WebPack , ARRUMAR!!

---
+++ public/script.js
@@ -0,0 +1,6 @@
+/**
+ * Babel : Converter (Transpirar) um código do React para um código que o browzer entenda
+ * WebPack : Para Cada Tipo de arquivo (.js , .css , .img) eu conveter meu cófigo de maneiras diferentes
+ * 
+ * Loaders : babel-loader , css-loader , img-loader
+ */
