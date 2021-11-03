98e5c033feaa165f85a0b1df6a200585dcadebf8
AllefyNepomucena <allefy.s.nepomucena@gmail.com>
2021-11-02 21:35:50 -0300

    Arrumando webpack !!!!!

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,70 @@
-const path = requise('path')
+const path = require('path')
 
 module.exports={
     entry : path.resolve(__dirname ,'src','index.js'),
     output:{
-        path: path.resolve(__dirnamedirname ,'public'),
+        path: path.resolve(__dirname ,'public'),
         filename: 'bundle.js'
     },
 
 
-   module:{
-        rules:[
-            {
-                test:/\.js$/,
-                exclude:/node_modules/,
-                use:{
-                    loader: 'babel-loader'
-                }
+    module: {
+       
+        rules: [
+         {
+            
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+           loader: 'babel-loader',
+           
+          }, 
+    
+         },
+         
+         ]
+         
+         }
+         }
+
+         
 
 
+// const path = require('path')
 
-           }
-    ]
-    }
+// module.exports = {
+//  entry: path.resolve(__dirname, 'src', 'index.js'),
+//  output: {
+//   path: path.resolve(__dirname, 'public'),
+//   filename: 'bundle.js'
+//  },
+//  devServer: {
+//   contentBase: path.resolve(__dirname, 'public'),
+//  },
+//  module: {
+//   rules: [
+//    {
+//     test: /\.js$/,
+//     exclude: /node_modules/,
+//     use: {
+//      loader: 'babel-loader',
+//     }
 
-}
+//    },
+//    {
+//     test: /\.css$/,
+//     exclude: /node_modules/,
+//     use: [
+//      { loader: 'style-loader' },
+//      { loader: 'css-loader' }
+//     ]
+//    },
+//    {
+//     test: /.*\.(gif|png|jpe?g)$/i,
+//     use: {
+//      loader: 'file-loader'
+//     }
+//    }
+//   ]
+//  }
+// }
